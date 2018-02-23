ddd6aed8f26e2061ad2f2a5c98a11b4479a1cda5
tonkytonky <as@asamsonov.me>
2018-02-23 21:01:46 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 # Coursera Dump
 
-[TODO. There will be project description]
+This is the script for dumping information about some number of arbitrary 
+courses from Coursera. For each course it will get its name, taught language, 
+the number of weeks it lasts and its average user rating.
+
+The output will be written to a specified XLSX file. 
+
+
+# Usage
+
+```bash
+coursera.py [-n NUMBER] path
+```
+`path` - path to output file  
+`-n NUMBER` - number of courses to get
+
 
 # Project Goals
 
-The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+The code is written for educational purposes. 
+Training course for web-developers - [DEVMAN.org](https://devman.org)
--- coursera.py
+++ coursera.py
@@ -1,16 +1,111 @@
+import argparse
+import random
+from os import path
+import xml.etree.ElementTree as ET
+
+from bs4 import BeautifulSoup
+from openpyxl import Workbook
+import requests
 
 
-def get_courses_list():
-    pass
+def _main():
+    parser = argparse.ArgumentParser()
+    args = get_args(parser)
+    if not path.exists(path.split(args.path)[0]):
+        parser.error('a path to write file not found, '
+                     'specify existing path in `path` argument')
+
+    courses_list = get_courses_list(args.number)
+    courses_info = [get_course_info(course) for course in courses_list]
+
+    output_courses_info_to_xlsx(courses_info, args.path)
 
 
-def get_course_info(course_slug):
-    pass
+def get_args(parser):
+    parser.add_argument(
+        'path',
+        help='Path to output file'
+    )
+    parser.add_argument(
+        '-n',
+        '--number',
+        help='Number of courses to get',
+        type=int,
+        default=20
+    )
+
+    return parser.parse_args()
 
 
-def output_courses_info_to_xlsx(filepath):
-    pass
+def get_courses_list(number):
+    print('Получение списка курсов...')
+    courses = requests.get(
+        'https://www.coursera.org/sitemap~www~courses.xml').text
+    courses = ET.fromstring(courses)
+    namespace = {'xmlns': "http://www.sitemaps.org/schemas/sitemap/0.9"}
+    courses_full_list = [
+        loc.text for loc in courses.findall('.//xmlns:loc', namespace)]
+
+    courses = set()
+    while len(courses) < number:
+        courses.add(random.choice(courses_full_list))
+
+    return list(courses)
+
+
+def get_course_info(course_url):
+    print('Получение информации по курсу: {}'.format(course_url))
+    course_response = requests.get(course_url)
+    course = BeautifulSoup(
+        course_response.content.decode('utf-8'), 'html.parser')
+
+    language_node = course.find('div', class_='rc-Language')
+    language = language_node.text if language_node else '-'
+
+    commitment_node = course.find('i', class_='cif-clock')
+    if commitment_node:
+        for parent in commitment_node.parents:
+            if parent.name == 'tr':
+                break
+        commitment = parent.find('td', class_='td-data').text
+    else:
+        commitment = '-'
+
+    stars_node = course.find('div', class_='ratings-info')
+    stars = stars_node.find_all('div')[1].text if stars_node else '-'
+
+    course_info = {
+        'name': course.h1.text,
+        'language': language,
+        'commitment': commitment,
+        'stars': stars,
+    }
+    print([item for item in course_info.items()])
+    return course_info
+
+
+def output_courses_info_to_xlsx(courses_info, filepath):
+    print('Запись в файл')
+    headers = [
+        'Название',
+        'Язык',
+        'Количество недель',
+        'Средняя оценка',
+    ]
+    wb = Workbook()
+    ws = wb.active
+    ws.append(headers)
+
+    for course_info in courses_info:
+        ws.append([
+            course_info['name'],
+            course_info['language'],
+            course_info['commitment'],
+            course_info['stars'],
+        ])
+
+    wb.save(filepath)
 
 
 if __name__ == '__main__':
-    pass
+    _main()
